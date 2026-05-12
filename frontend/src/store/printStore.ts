d25c22197edc9a025058357fc189a69788c6153c
santimat <santimaturo1@gmail.com>
2026-05-12 01:35:53 -0300

    refactor: restructure Product interfaces and update example product details

--- frontend/src/store/printStore.ts
+++ frontend/src/store/printStore.ts
@@ -3,19 +3,25 @@ import { persist } from "zustand/middleware";
 import type { Product } from "@lib/types";
 import { useDefaultConfigStore } from "@store/defaultConfigStore.ts";
 
-interface ProductStore {
+interface ProductState {
   products: Product[];
+}
+
+interface ProductActions {
   addPrint: (product: Product) => void;
   findProductById: (id: string) => Product | null;
 }
+
+interface ProductStore extends ProductState, ProductActions {}
 
 const exampleProduct: Product = {
   id: "1",
   name: "Producto de ejemplo",
   material: "PLA",
   materialAmount: 100,
-  printTime: 60,
-  img: "https://via.placeholder.com/150",
+  printingHours: 1,
+  printingMinutes: 30,
+  img: "https://i.pinimg.com/1200x/ee/c2/93/eec293754c9ce69e4c88cc8ac9be27d0.jpg",
   createdAt: new Date(),
 };
 
@@ -24,11 +30,6 @@ export const useProductStore = create<ProductStore>()(
   persist(
     (set, get) => ({
       products: [exampleProduct],
-      getMaterialCost: (product: Product) => {
-        // .getState() gives the value without suscribe to changes, prevents unnecessary re-renders
-        const { materialPrice } = useDefaultConfigStore.getState();
-        return product.materialAmount * (materialPrice || 0);
-      },
       addPrint: (product: Product) => {
         const { products } = get();
         set({ products: [...products, product] });
